71cd10d80ab14dadd46cfb7b1fd23a7ea320a822
unknown <xenmoraitis@gmail.com>
2021-05-15 14:57:00 +0200

    netlify functions no build

--- .netlify/functions-serve/helloWorld/src/helloWorld.js
+++
@@ -1,8 +0,0 @@
-// netlify/functions/helloWorld.js
-exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({message: "Hello World"})
-  };
-};
-//# sourceMappingURL=helloWorld.js.map
