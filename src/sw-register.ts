92e3cf2b474e8e114b9147e7b75a9e8d85d7f80c
Fontas <xenmoraitis@gmail.com>
2023-12-12 19:28:45 +0100

    updating service worker

--- src/sw-register.ts
+++ src/sw-register.ts
@@ -1,9 +1,5 @@
 const registerServiceWorker = () => {
-  // if ('serviceWorker' in navigator) {
-  //   navigator.serviceWorker.register('./serviceworker.js');
-  // }
-
-  if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+  if (navigator.serviceWorker && process.env.NODE_ENV === 'production') {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/sw.js');
     });
